bac5ac16d125f8083c41ad26630cd4df8cedcbb8
Scott Andrews <sandrews@pivotal.io>
2020-05-13 11:04:17 -0400

    Drop GCS ACL since the bucket has a uniform access policy now

--- .github/workflows/gcs-publish.sh
+++ .github/workflows/gcs-publish.sh
@@ -7,6 +7,6 @@ set -o pipefail
 version=$(cat VERSION)
 git_sha=$(git rev-parse HEAD)
 
-gsutil cp -a public-read command-function-invoker.tgz gs://projectriff/command-function-invoker/command-function-invoker-linux-amd64-${version}/snapshot/${git_sha}.tgz
-gsutil cp -a public-read command-function-invoker.tgz gs://projectriff/command-function-invoker/command-function-invoker-linux-amd64-${version}.tgz
-gsutil cp -a public-read command-function-invoker.tgz gs://projectriff/command-function-invoker/command-function-invoker-linux-amd64-latest.tgz
+gsutil cp command-function-invoker.tgz gs://projectriff/command-function-invoker/command-function-invoker-linux-amd64-${version}/snapshot/${git_sha}.tgz
+gsutil cp command-function-invoker.tgz gs://projectriff/command-function-invoker/command-function-invoker-linux-amd64-${version}.tgz
+gsutil cp command-function-invoker.tgz gs://projectriff/command-function-invoker/command-function-invoker-linux-amd64-latest.tgz
